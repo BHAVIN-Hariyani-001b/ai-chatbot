5917e6d6652c0d3f7779fe392f53c3258e14c282
BHAVIN-Hariyani-001b <bhedit367@gmail.com>
2025-09-14 10:30:26 +0530

    add function in app.js and desing improve

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,37 @@
 const sendButton = document.getElementById("send-button");
+const userInput =  document.querySelector('#user-input');
+const send_Message = document.querySelector("#sendmessage");
 sendButton.addEventListener("click", sendMessage);
 
-document.querySelector('#user-input').addEventListener("keydown",(event)=>{
+
+// enter button click and send ms 
+userInput.addEventListener("keydown",(event)=>{
   if(event.key === "Enter"){
     sendMessage();
   }
 });
 
+// Move input container to bottom 
+function moveInputToBottom(){
+  send_Message.classList.remove("center");
+  send_Message.classList.add("bottom");
+  userInput.focus();
+}
+
+// api fetch data and data is fill in the html container
 async function sendMessage() {
+  moveInputToBottom();
   let input = document.getElementById("user-input");
   let message = input.value;
   if (message.trim() === "") return;
   input.value = "";
 
   let chatBox = document.getElementById("chat-box");
-  chatBox.innerHTML += `<div class="user">You: ${message}</div>`;
+  chatBox.innerHTML += `<div class="user">
+                          <div class="user_chat">
+                            ${message}
+                          </div>
+                        </div>`;
 
   const response = await fetch("/chat", {
     method: "POST",
@@ -25,7 +42,6 @@ async function sendMessage() {
   const data = await response.json();
   // console.log(data);
 
-  chatBox.innerHTML += `<div class="bot">Bot: ${data.reply}</div>`;
+  chatBox.innerHTML += `<div class="bot">${data.reply}</div>`;
   chatBox.scrollTop = chatBox.scrollHeight;
 }
-
