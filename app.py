d7eb058beb09d8a563edbbc379485e724dd9f5c9
BHAVIN-Hariyani-001b <bhedit367@gmail.com>
2025-09-20 13:27:58 +0530

    edit file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from google import genai
 from flask import Flask, render_template, request, jsonify
+import re
+import markdown
 
 app = Flask(__name__)
 
@@ -13,7 +15,21 @@ def chatbot_response(user_text):
     except Exception as e:
         return f"Error : {e}"
  
-    return response.text
+    # return response.text
+    return format_response(response.text)
+
+
+def format_response(raw_text):
+    formatted = raw_text.strip()
+
+    # your regex cleanup (optional)
+    formatted = raw_text.strip()
+    formatted = re.sub(r"^\s*(\d+)\.\s*", r"## 🔹 \1. ", formatted, flags=re.MULTILINE)
+    formatted = re.sub(r"^\s*-\s*", r"- ", formatted, flags=re.MULTILINE)
+    formatted = re.sub(r"([.!?])\s+", r"\1\n\n", formatted)
+
+    return markdown.markdown(formatted)
+
 
 @app.route("/")
 def home():
--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,18 @@
 const sendButton = document.getElementById("send-button");
-const userInput =  document.querySelector('#user-input');
+const userInput = document.querySelector("#user-input");
 const send_Message = document.querySelector("#sendmessage");
 sendButton.addEventListener("click", sendMessage);
 
-
-// enter button click and send ms 
-userInput.addEventListener("keydown",(event)=>{
-  if(event.key === "Enter"){
+// enter button click and send ms
+userInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && !sendButton.disabled) {
     sendMessage();
     sendButton.disabled = true;
   }
 });
 
-// Move input container to bottom 
-function moveInputToBottom(){
+// Move input container to bottom
+function moveInputToBottom() {
   send_Message.classList.remove("center");
   send_Message.classList.add("bottom");
   userInput.focus();
@@ -22,6 +21,12 @@ function moveInputToBottom(){
 // api fetch data and data is fill in the html container
 async function sendMessage() {
   moveInputToBottom();
+
+  let welcomeMessage = document.querySelector(".welcome");
+  if(welcomeMessage){
+    send_Message.removeChild(welcomeMessage);
+  }
+
   let input = document.getElementById("user-input");
   let message = input.value;
   if (message.trim() === "") return;
@@ -33,17 +38,36 @@ async function sendMessage() {
                             ${message}
                           </div>
                         </div>`;
-
-  const response = await fetch("/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: message }),
-  });
-
-  const data = await response.json();
-  // console.log(data);
-
-  chatBox.innerHTML += `<div class="bot">${data.reply}</div>`;
   chatBox.scrollTop = chatBox.scrollHeight;
+
+  // animation
+  let animation = document.createElement("div");
+  animation.innerHTML = `
+    <div class="animation">
+      <div class="sub"></div>
+      <div class="sub"></div>
+      <div class="sub"></div>
+    </div>
+  `;
+  chatBox.appendChild(animation);
+  chatBox.scrollTop = chatBox.scrollHeight;
+
+  try {
+    const response = await fetch("/chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: message }),
+    });
+    const data = await response.json();
+    // console.log(data);
+    chatBox.removeChild(animation);
+    chatBox.innerHTML;
+    chatBox.innerHTML += `<div class="bot">${data.reply}</div>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
+  } catch (err) {
+    chatBox.removeChild(animation);
+    chatBox.innerHTML += `<div class="bot">Try Again</div>`;
+  }
+
   sendButton.disabled = false;
 }
